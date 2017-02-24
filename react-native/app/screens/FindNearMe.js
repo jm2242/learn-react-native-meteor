b0297357e98c7f708113174202254a9084038b09
Jonathan Mares <jm2242@cornell.edu>
2017-02-23 20:28:19 -0500

    added find near me button and get location for ios

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
-
+import LocateMeButton from '../components/LocateMeButton';
+import config from '../config/config';
 class FindNearMe extends Component {
   static route = {
     navigationBar: {
@@ -9,11 +10,36 @@ class FindNearMe extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  handleGeolocationSuccess = (position) => {
+    console.log(position);
+  };
+
+  // Not sure why error handling not being called
+  handleGeolocationError = (error) => {
+    this.props.navigator.showLocalAlert(error.message, config.errorStyles);
+  };
+
+  // pro tip, using this syntax handles all necessary binding
+  // of thisso all functions in this JS class use the same this
+  // it's equivalent to onPress={functionName.bind(this)}
+  goToNearMe = () => {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     return (
       <Container>
+        <LocateMeButton onPress={this.goToNearMe} />
         <Header>
-          Find Near Me
+          Find Nearest Charging Stations
         </Header>
       </Container>
     );
